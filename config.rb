9c21c0cb0f3a931e5488e7989ea6471d0e974890
Daniel Burka <danielburka@gmail.com>
2019-02-21 21:05:03 +0530

    Blog added

--- config.rb
+++ config.rb
@@ -1,60 +1,71 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+###
+# Page options, layouts, aliases and proxies
+###
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
-
-activate :i18n
-
-activate :directory_indexes
-
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
+# Per-page layout changes:
+#
+# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# Use relative URLs for assets (to support GitHub Pages)
-activate :relative_assets
-set :relative_links, true
-
 # With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+# page "/path/to/file.html", layout: :otherlayout
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
+###
 # Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
+###
 
-helpers do
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
 
-  def global_current_url
-    segments = current_page.url.split('/').reject { |segment| segment.empty? }
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
+  # blog.summary_separator = /()/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
 
-    # Clear out the language prefix if present
-    segments.shift if langs.map(&:to_s).include?(segments.first)
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
 
-    segments.join("/")
-  end
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
 end
 
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
+page "/feed.xml", layout: false
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
 # end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
+
+require 'tzinfo'
+Time.zone = 'US/Pacific'
