bc2299000f8298855b143802117062cb6618c665
Daniel Burka <danielburka@gmail.com>
2019-02-21 16:29:08 +0530

    Big changes

---
+++ README.md
@@ -0,0 +1,18 @@
+# ElyceCole.com
+
+This repository houses the source code for the ElyceCole.com app website.
+
+## Development
+
+We use [Middleman](https://middlemanapp.com/) to build the static pages which are hosted on [GitHub Pages](https://pages.github.com/).
+
+To run the site on your own machine, simply clone the repo, install the packages necessary, and start up a Middleman server.
+
+```
+$ git clone git@github.com:dburka/elycecole.git
+$ cd elycecole
+$ bundle install
+$ middleman server
+```
+
+Now you should be able to go to http://localhost:4567 and see the site running locally.
---
+++ config.rb
@@ -0,0 +1,60 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :i18n
+
+activate :directory_indexes
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# Use relative URLs for assets (to support GitHub Pages)
+activate :relative_assets
+set :relative_links, true
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+helpers do
+
+  def global_current_url
+    segments = current_page.url.split('/').reject { |segment| segment.empty? }
+
+    # Clear out the language prefix if present
+    segments.shift if langs.map(&:to_s).include?(segments.first)
+
+    segments.join("/")
+  end
+end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
