7c6380eb1375763dc807ea37096cdcc0398c0970
Daniel Burka <danielburka@gmail.com>
2024-11-21 13:15:22 +0000

    Better footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,24 @@ export function Footer() {
   return (
     <>
       <section className="left">
-
-        <h3>Office</h3>
-        <p>We have an office in the Shoreditch neighborhood of London which has a <Link href="/coworking">co-working space</Link> for people who work on hard problems: <Link href="https://maps.app.goo.gl/8SYY1vdDDcwwqGJy7">1 Rivington Pl, London EC2A 3BA</Link></p>
-
-        <h3 className="space-top-small">Code of conduct &amp; privacy</h3>
-        <p>Our online and in-person events and co-working space follow <Link href="/conduct">our code of conduct</Link>. Also see our <Link href="/privacy">privacy notice</Link> for how we handle personal information.</p>
-
-        <h3 className="space-top-small">Contact</h3>
-        <p>Email: contact@hardproblems.com</p>
+        <div className="columns-2">
+          <div className="column">
+            <h3>Office</h3>
+            <p><Link href="https://maps.app.goo.gl/8SYY1vdDDcwwqGJy7">1 Rivington Pl, London EC2A 3BA</Link></p>
+            <p><Link href="/coworking">Co-working space</Link></p>
+            <h3 className="space-top-small">Contact</h3>
+            <p><Link href="mailto:contact@hardproblems.com">contact@hardproblems.com</Link></p>
+          </div>
+          <div className="column">
+            <h3>Legal &amp; Code of conduct</h3>
+            <p><Link href="/conduct">Code of conduct</Link></p>
+            <p><Link href="/privacy">Privacy notice</Link></p>
+            <p>&copy; Copyright {new Date().getFullYear()} Hard Problems</p>
+          </div>
+        </div>
       </section>
       <section className="right">
-        <h3>Legal</h3>
-        <p>&copy; Copyright {new Date().getFullYear()} Hard Problems</p>
+        <h3>Registration</h3>
         <p>
           Hard Problems is a non-profit Company Limited by Guarantee registered
           with{' '}
