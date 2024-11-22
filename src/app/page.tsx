bde334a4289eb4e6b16bc62e701595e77d68d330
Daniel Burka <danielburka@gmail.com>
2024-11-22 14:57:37 +0000

    Content updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           We are a non-profit that helps tech people to work on the hard problems that matter in the
           world: problems like <em>public health</em>, <em>climate change</em>, and <em>good government</em>.
         </p>
-        <p>While Silicon Valley is focused on AI, VR, crypto, SAAS, fin-tech, and ad-tech, many people in the real world are trying to tackle the hardest, thorniest problems.</p>
+        <p>While Silicon Valley is focused on AI, VR, SAAS, crypto, fin-tech, and ad-tech, many people in the real world are trying to tackle the hardest, thorniest problems.</p>
         <p>Programmers and designers know these issues matter but they wring their hands and stand by, unsure how to have any positive impact… choosing to focus on making big money and working on trendy new technologies.</p>
         <p>It’s time to refocus the tech world on what matters most. It’s time to build new relations between doctors, environmentalists, scientists, not-for-profit leaders, public servants, and others tackling the world’s hardest problems with technologists who can help make practical tools to help solve them.</p>
         <p>We are a multi-skilled team of very experienced tech veterans and other subject-matter experts. We aim to push this effort &#8212; whether it’s by building teams to work on issues, creating bridges between ambitious experts and technologists, finding ways to fund tech initiatives, or inspiring a generation of tech people to work on these fundamentally important problems.</p>
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,20 +8,25 @@ export function Team() {
 
       <div>
         <Link href="https://www.linkedin.com/in/elyce-cole-1380903a/">
-        <Image src="/images/team/elyce.svg" width="80" height="80" alt="" /> Elyce
-        Cole
+          <Image src="/images/team/elyce.svg" width="80" height="80" alt="" />
+          ElyceCole
         </Link>
       </div>
 
       <div>
         <Link href="https://www.linkedin.com/in/dburka/">
-        <Image src="/images/team/daniel.svg" width="80" height="80" alt="" /> Daniel Burka
+          <Image src="/images/team/daniel.svg" width="80" height="80" alt="" />
+          Daniel Burka
         </Link>
       </div>
 
       <div>
-        <Image src="/images/team/blank.svg" width="80" height="80" alt="" /> Coming soon...
+        <Link href="https://www.linkedin.com/in/mahima-chandak/">
+          <Image src="/images/team/mahima.svg" width="80" height="80" alt="" />
+          Mahima Chandak
+        </Link>
       </div>
+
       <div>
         <Image src="/images/team/blank.svg" width="80" height="80" alt="" /> Coming soon...
       </div>
