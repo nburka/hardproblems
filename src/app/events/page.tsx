52b168541b6f05e9edb061c98181e4c51ecc6df0
Daniel Burka <danielburka@gmail.com>
2025-03-12 17:31:22 +0000

    Fixed quotes

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
             <div className="event-image-date">Mar 13, 2025</div>
           </div>
           <h4><Link href="https://lu.ma/bengeegp">Fireside chat with Brian O&#39;Donnell from DHIS2.org</Link></h4>
-          <p>We are hosting tech architect, Brian O'Donnell (Linkedin), for a fireside chat where we will discuss what he does, how he ended up working on DHIS2.org, and the highs and lows of working at the intersection of academia, government, global public health, and tech. This will be an open discussion where you can ask Brian questions.</p>
+          <p>We are hosting tech architect, Brian O D&rsquo;onnell (Linkedin), for a fireside chat where we will discuss what he does, how he ended up working on DHIS2.org, and the highs and lows of working at the intersection of academia, government, global public health, and tech. This will be an open discussion where you can ask Brian questions.</p>
           <dl className="event-details">
             <dt>When?</dt>
             <dd>6-9 pm on Thur, March 13, 2025</dd>
@@ -42,7 +42,7 @@ export default function Page() {
             <div className="event-image-date">Apr 10, 2025</div>
           </div>
           <h4><Link href="https://lu.ma/kme2kbaa">Fireside chat with ASquared: Can a design agency do good in the world and run a viable business?</Link></h4>
-          <p>We will discuss with three of the leaders of ASquared about their agency's transition to a B-Corp that does half their work with organizations working on "Sustainable Development Goals." Is it possible to run a successful digital agency with an explicit aim to make the world better?</p>
+          <p>We will discuss with three of the leaders of ASquared about their agency&rsquo;s transition to a B-Corp that does half their work with organizations working on &ldquo;Sustainable Development Goals.&rdquo; Is it possible to run a successful digital agency with an explicit aim to make the world better?</p>
           <dl className="event-details">
             <dt>When?</dt>
             <dd>6-9 pm on Thur, April 10, 2025</dd>
